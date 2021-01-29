39dfc8b9f3ccd0748fbbe7e66b707e464c7cabe9
facecheckdisnuts <omranimahmoud@gmail.com>
2021-01-29 13:06:34 +0100

    add calendar + changes ui on dash

--- src/Components/CustomCard.js
+++ src/Components/CustomCard.js
@@ -1,21 +1,37 @@
 import { Divider, Grid, Paper } from '@material-ui/core'
 import React from 'react'
 import LocalMallIcon from '@material-ui/icons/LocalMall';
+import {motion} from 'framer-motion'
 function CustomCard({number,type,children}) {
+
+    const contentVariant = {
+        hidden:{
+            scale:0,
+        },
+        visible:{
+            scale:1,
+            transition:{
+                type:'tween',
+                duration:0.4,  
+            }
+        },   
+    }
     return (
-        <Grid item md = {3} style={{borderRadius:'10px',marginLeft:'15px',height:'153px'}}>
-            <Paper elevation={3} style={{display:'flex',height:'153px',borderRadius:'10px',padding:'10px',background:'rgb(243,245,247)',flexDirection:'column'}}>
-            <section style={{width:'100%',display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',height:'60%',marginBottom:'5px'}}>
-                    <div style={{background:'rgb(36,38,76)',width:'60px',height:'60px',borderRadius:'10px',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
+        <Grid item md = {3} style={{borderRadius:'10px',marginLeft:'15px',height:'130px'}}>
+            <Paper elevation={3} style={{display:'flex',height:'130px',borderRadius:'10px',padding:'10px',background:'rgb(243,245,247)'}}>
+                <section style={{width:'75%',display:'flex',flexDirection:'column',justifyContent:'center',height:'100%',alignItems:'center',marginTop:'5px'}}>
+                        <span style={{paddingLeft:'10px',fontWeight:'bold',fontSize:'25px',color:'#303030'}}>{number}</span>
+                        <span style={{paddingLeft:'10px',fontSize:'13px',color:'#303030',opacity:'60%'}}><b>{type}</b></span>
+                </section>
+                
+                <section style={{width:'25%',display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',height:'100%',marginBottom:'5px'}}>
+                    <div style={{background:'white',width:'60px',height:'60px',borderRadius:'100%',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}}>
                         {/* <LocalMallIcon style={{fontSize:'40px',color:'white'}}/> */}
                         {children}
                     </div>
                 </section>
-                <Divider variant="middle" />
-                <section style={{width:'100%',display:'flex',flexDirection:'column',justifyContent:'space-around',height:'40%',alignItems:'center',marginTop:'5px'}}>
-                        <span style={{paddingLeft:'10px',fontWeight:'bold',fontSize:'25px',color:'#303030'}}>{number}</span>
-                        <span style={{paddingLeft:'10px',fontSize:'13px',color:'#303030',opacity:'60%'}}><b>{type}</b></span>
-                </section>
+                {/* <Divider variant="middle" />
+                */}
                 
             </Paper>
         </Grid>
--- src/Pages/Home/Overview.js
+++ src/Pages/Home/Overview.js
@@ -1,5 +1,5 @@
-import { Container, Grid, Paper } from '@material-ui/core'
-import React from 'react'
+import { Button, Container, Grid, Paper } from '@material-ui/core'
+import React,{useEffect,useState,useRef} from 'react'
 import {ConnectedUser}  from '../../App'
 import CustomCard from '../../Components/CustomCard'
 import LocalMallIcon from '@material-ui/icons/LocalMall';
@@ -9,18 +9,53 @@ import { Doughnut, Line } from 'react-chartjs-2';
 import useMonth from '../../Hooks/useMonth'
 import UpdateIcon from '@material-ui/icons/Update';
 import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+
+function useOutsideAlerter(ref, toggleDate) {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          toggleDate(false)
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
 function Overview() {
+    const wrapperRef = useRef(null);
+    const [toggleDate, setToggleDate] = useState(false);
+    const [currentFocus, setCurrentFocus] = useState([]);
+    const [chosedDate, setChosedDate] = useState([{
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection'
+      }]);
+    useOutsideAlerter(wrapperRef, setToggleDate);
+
     const context = React.useContext(ConnectedUser);
     const data = {
-        labels: ['Day one', 'Day two', 'Day three', 'Day four','Day one', 'Day two', 'Day three', 'Day four','Day one', 'Day two', 'Day three', 'Day four','Day one', 'Day two', 'Day three'],
+        labels: ['13/02', '14/02', '15/02', '16/02','17/02', '18/02', '19/02', '20/02','21/02', '22/02', '23/02', '24/02','25/02', '26/02', '27/02'],
         datasets: [
           {
             label: '# Orders this day',
             data: [12, 19, 3, 5, 2, 3,12, 19, 3, 5, 2, 3,12, 19, 3],
-            fill: true,
+            fill: false,
             backgroundColor: 'rgba(36,38,76,0.3)',
             borderColor: 'rgb(36,38,76)',
           },
+          
         ],
       };
 
@@ -31,11 +66,35 @@ function Overview() {
             label: '# Client Orders',
             data: [8, 19, 53, 15],
             fill: false,
-            backgroundColor: ['white','rgb(112,95,251)','rgb(77,201,184)','rgb(245,105,119)'],
-            borderColor: ['white','rgb(112,95,251)','rgb(77,201,184)','rgb(245,105,119)'],
+            backgroundColor: ['#16193B','#35478C','#4E7AC7','#7FB2F0'],
+            borderColor: ['#16193B','#35478C','#4E7AC7','#7FB2F0'],
           },
         ],
       };
+      var optionsLine = {
+        responsive:true,
+        // legend: {
+        //     labels: {
+        //        fontSize:12,
+        //        fontColor:'rgb(36,38,76)'
+        //     }
+        // },
+        legend:false,        
+
+        
+        scales: {
+           
+            yAxes: [{
+                gridLines: {
+                    color: "rgba(0, 0, 0, 0)",
+                }   
+            }]
+        }
+    }
+    var optionsPie = {
+        responsive:true,
+        legend:false,        
+    }
 
     //   const options = {
     //     scales: {
@@ -49,6 +108,13 @@ function Overview() {
     //     },
     //   }
 
+    const [state, setState] = React.useState([
+        {
+          startDate: new Date(),
+          endDate: null,
+          key: 'selection'
+        }
+      ])
 
     console.log('MONTH IS ======>',useMonth())
       
@@ -58,40 +124,97 @@ function Overview() {
 
                 <Grid item md={12} style={{display:'flex',maxHeight:'153px'}}> 
                     <CustomCard type='Unfulfilled Orders' number={2408}>
-                        <LocalMallIcon style={{fontSize:'40px',color:'white'}}/>
+                        <LocalMallIcon style={{fontSize:'40px',color:'rgb(36,38,76)'}}/>
                     </CustomCard>
                     <CustomCard type='Refused Products' number={105}>
-                        <SyncDisabledIcon style={{fontSize:'40px',color:'white'}}/>
+                        <SyncDisabledIcon style={{fontSize:'40px',color:'rgb(36,38,76)'}}/>
                     </CustomCard>
                     <CustomCard type='Pending Products' number={301}>
-                        <SyncProblemIcon style={{fontSize:'40px',color:'white'}}/>
+                        <SyncProblemIcon style={{fontSize:'40px',color:'rgb(36,38,76)'}}/>
                     </CustomCard>
-                    <CustomCard type='Average order treatment time' number='3 Days'>
-                        <UpdateIcon style={{fontSize:'40px',color:'white'}}/>
+                    <CustomCard type='Order treatment time' number='3 Days'>
+                        <UpdateIcon style={{fontSize:'40px',color:'rgb(36,38,76)'}}/>
                     </CustomCard>
                 </Grid>
-                <div style={{display:'flex',marginTop:'35px',justifyContent:'space-around'}}>
-                    <Grid item md={5}> 
-                        <h3 style={{color:'rgb(36,38,76)'}}>Last 15 days Orders </h3>
-                        <Paper elevation={3} style={{display:'flex',height:'285px',borderRadius:'10px',padding:'10px',background:'rgb(243,245,247)'}}>
+                <div ref={wrapperRef} style={{marginTop:'50px'}}>
+            <Button style={{background:'#35478C',color:'white'}} variant='contained' onClick={() => setToggleDate(!toggleDate)} >
+
+              {chosedDate[0] !== null
+                ? [
+                  chosedDate[0].startDate.toLocaleString("en", {
+                    month: "short",
+                  }),
+                  " ",
+                  chosedDate[0].startDate.getDate(),
+                  " ",
+                  chosedDate[0].startDate.getFullYear(),
+                  "  -  ",
+                  chosedDate[0].endDate && chosedDate[0].endDate.toLocaleString("en", {
+                    month: "short",
+                  }),
+                  " ",
+                  chosedDate[0].endDate && chosedDate[0].endDate.getDate(),
+                  " ",
+                  chosedDate[0].endDate && chosedDate[0].endDate.getFullYear(),
+                ]
+                : null}
+            </Button>
+                {toggleDate && ( 
+                <div style={{ position: "absolute" }}>
+                <div className="calenderDivStyle">
+                    <div className="btnsCalendar">
+                        <DateRange
+                            editableDateInputs={true}
+                            moveRangeOnFirstSelection={false}
+                            onRangeFocusChange={item => { setCurrentFocus(item) }}
+                            onChange={item => { setChosedDate([item.selection]); }}
+                            moveRangeOnFirstSelection={false}
+                            ranges={chosedDate}
+                            dragSelectionEnabled={true}
+                            rangeColors={["#35478C"]}
+                        />
+                    </div>
+                </div>
+                </div>)}
+                </div>
+               
+                <div style={{display:'flex',marginTop:'25px',justifyContent:'space-around'}}>
+                    <Grid item md={6} style={{padding:'10px'}}> 
+                        {/* <h3 style={{color:'rgb(36,38,76)'}}>Last 15 days Orders </h3> */}
+                        <Paper elevation={3} style={{display:'flex',height:'300px',borderRadius:'10px',background:'rgb(243,245,247)',flexDirection:'column',padding:'10px 10px 30px 10px'}}>
+                            <section style={{display:'flex',width:'100%',justifyContent:'space-between',alignItems:'center',height:'30px',marginBottom:'10px'}}>
+                                <h3 style={{color:'rgb(36,38,76)'}}>Last 15 days Orders </h3>
+                                <section style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+                                    <div style={{background:'rgb(36,38,76)',width:'7px',height:'7px',borderRadius:'100%',marginRight:'5px'}}></div>
+                                    <span style={{fontSize:'13px',color:'rgb(36,38,76)'}}>Orders this day</span>
+                                </section>
+                            </section>
                             <Line
-                                // options={options}
+                                options={optionsLine}
                                 redraw
                                 data={data}
                             />
                         </Paper>
                     </Grid>
 
-                    <Grid item md={5}>
+                    <Grid item md={6} style={{padding:'10px'}}>
  
-                    <div style={{height:'20px',alignItems:'center',display:'flex',padding:'8px',width:'90%',justifyContent:'center',marginTop:'20px'}}>
+                        <Paper elevation={3} style={{display:'flex',height:'300px',borderRadius:'10px',background:'rgb(243,245,247)',flexDirection:'column',padding:'10px 10px 30px 10px'}}>
+                           
+                    <div style={{height:'20px',alignItems:'center',display:'flex',padding:'8px',width:'90%',justifyContent:'flex-start'}}>
                                 <SupervisedUserCircleIcon  style={{marginRight:'10px',color:'rgb(36,38,76)',fontSize:'30px'}}/>
                                 <span style={{fontSize:'20px',color:'#303030',color:'rgb(36,38,76)'}}><b>Top 4 Clients</b></span>
                             </div>
-                        <Paper elevation={3} style={{display:'flex',height:'285px',borderRadius:'10px',padding:'10px',background:'rgb(243,245,247)',flexDirection:'column',marginTop:'18px'}}>
-                           
-                            <Grid item md={12} style={{marginTop:'10px'}}>
-                                <Doughnut data={data2} />
+                            <Grid item md={12} style={{marginTop:'10px',display:'flex'}}>
+                                <secion style={{width:'75%',justifyContent:'center',alignItems:'center',display:'flex'}}>
+                                    <Doughnut data={data2} options={optionsPie}/>
+                                </secion>
+                                <section style={{width:'25%',justifyContent:'center',alignItems:'flex-start',display:'flex',flexDirection:'column'}}>
+                                    <li  style={{color:'#16193B',fontSize:'25px',listStylePosition:'inside'}}><span style={{fontSize:'15px',color:'rgb(36,38,76)'}}>Client one</span></li>
+                                    <li  style={{color:'#35478C',fontSize:'25px'}}><span style={{fontSize:'15px',color:'rgb(36,38,76)'}}>Client two</span></li>
+                                    <li  style={{color:'#4E7AC7',fontSize:'25px'}}><span style={{fontSize:'15px',color:'rgb(36,38,76)'}}>Client three</span></li>
+                                    <li  style={{color:'#7FB2F0',fontSize:'25px'}}><span style={{fontSize:'15px',color:'rgb(36,38,76)'}}>Client four</span></li>
+                                </section>
                             </Grid>
                         </Paper>
                     </Grid>
